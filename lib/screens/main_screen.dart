8d93fa28f98c135457fa1004948341a0e262a5f0
unknown <manojbk488@gmail.com>
2022-07-07 10:25:16 +0545

    created project

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Rider App',
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
